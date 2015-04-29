3292b588566e680f127a99ef79221d6cebb51ca2
Albert Palacios <apalacios@modpow.es>
2015-04-29 18:02:14 +0200

    Added comment

--- sortArray-example.js
+++ sortArray-example.js
@@ -14,7 +14,7 @@ console.log("This example shows how to use the 'SortArray' object to ");
 console.log("sort one 'array' of 'strings' in a non blocking manner.");
 console.log("\nOriginal array:");
 console.log(arr);
-console.log("\nSorted array:");
+console.log("\nSorted (non blocking) array:");
 SortArray.run(arr, function (rst) {
     'use strict';
     console.log(rst);
